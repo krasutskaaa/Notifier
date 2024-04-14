030c3d7f043ffe4985632c1587be445351715d31
krasutskaaa <krasutskadiana@gmail.com>
2024-04-14 22:53:47 +0300

    a form for a notification timer setting was added with a created design

---
+++ Dashboard/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notifier.Dashboard
+{
+    public partial class TimePicker : Form
+    {
+        public TimePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
