1b7aa88f2ff01ad6481d0581cefa0a7a217b0609
krasutskaaa <krasutskadiana@gmail.com>
2024-04-16 09:50:05 +0300

    an Entity interface was added and entities in form of List and Notification were implemented

---
+++ Abstractions/Entities/List.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifier.Entities;
+
+public class List
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+   
+}
---
+++ Abstractions/Entities/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifier.Entities;
+
+public class Notification
+{
+    public Guid Id { get; set; }
+    public Guid ListId { get; init; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTime { get; set;}
+    public bool IsImportant { get; set; }
+
+}
