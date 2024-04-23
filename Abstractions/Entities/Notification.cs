8f5dc560caceb254ae5bb129e0cb1c1d07f597f8
krasutskaaa <krasutskadiana@gmail.com>
2024-04-23 15:37:27 +0300

    The Notification entity was changed according to the Entity inteface and new fields were added

--- Abstractions/Entities/Notification.cs
+++ Abstractions/Entities/Notification.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Notifier.Entities;
+namespace Notifier.Abstractions.Entities;
 
-public class Notification
+public class Notification: Entity
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; init; } = Guid.NewGuid();
     public Guid ListId { get; init; }
     public string Name { get; set; }
-    public string Description { get; set; }
-    public DateTime DateTime { get; set;}
+    public string? Description { get; set; }
+    public DateTime DateTime { get; set; }
     public bool IsImportant { get; set; }
+    public bool IsDone { get; set; }
+    public override string ToString()
+        => $"{Name}";
 
 }
