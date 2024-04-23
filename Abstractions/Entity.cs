6caefdf76c40152b36afb55317414e7a36820000
krasutskaaa <krasutskadiana@gmail.com>
2024-04-23 15:33:01 +0300

    repeated adding Entity interface with a changed Guid field

--- Abstractions/Entity.cs
+++ Abstractions/Entity.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Notifier.Abstractions
+namespace Notifier.Abstractions;
+
+public interface Entity
 {
-    internal interface Entity
-    {
-    }
+    public Guid Id { get; init; }
+    public string Name { get; set; }
 }
